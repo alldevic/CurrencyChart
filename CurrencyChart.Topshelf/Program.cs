6295a892aa84cb956cf764bbc4f3903ef7f6ccf7
Nikolay Bely <belyynikolay@gmail.com>
2019-04-19 02:20:46 +0700

    Add NLog support

--- CurrencyChart.Topshelf/Program.cs
+++ CurrencyChart.Topshelf/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using NLog;
 
 namespace CurrencyChart.Topshelf
 {
     internal class Program
     {
+        static readonly Logger  Log = LogManager.GetCurrentClassLogger();
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            Log.Info("Hello world!");
             Console.ReadKey();
         }
     }
